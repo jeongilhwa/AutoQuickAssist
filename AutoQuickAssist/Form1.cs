2783f6b7a33fd54412644424a45b51d11a0bf61e
jeong <jeong@DESKTOP-SUPT8TI>
2021-12-14 00:24:02 +0900

    초기 커밋.

---
+++ AutoQuickAssist/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+using System.Security.Principal;
+
+
+namespace AutoQuickAssist
+{   
+    public partial class Form1 : Form
+    {
+        //  Mouse autoClickMouse { get; set; } = new Mouse();
+        //public bool _systemShutdown = true;
+        QuickAssistProsess QuickAssistProsess = new QuickAssistProsess();        
+        int timerCount;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadQuickAutoLoginTextFile();
+
+            //if (!IsRunningAsAdministrator())
+            //{
+            //    ProcessStartInfo processStartInfo = new ProcessStartInfo(Assembly.GetEntryAssembly().CodeBase);
+            //    {
+            //        var withBlock = processStartInfo;
+            //        withBlock.UseShellExecute = true;
+            //        withBlock.Verb = "runas";
+            //        Process.Start(processStartInfo);
+            //        Application.Exit();
+            //    }
+            //}
+            //else
+            //    this.Text += " " + "(Administrator)";
+        }
+        public bool IsRunningAsAdministrator()
+        {
+            WindowsIdentity windowsIdentity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal windowsPrincipal = new WindowsPrincipal(windowsIdentity);
+            return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void ReadQuickAutoLoginTextFile()
+        {
+            string mydoc = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (File.Exists(mydoc + @"\\QuickAutoLogin.txt"))
+            {
+                string lines = File.ReadAllText(mydoc + @"\\QuickAutoLogin.txt");
+                QuickAssistIDTextBox.Text = lines;
+                QuickAssistProsess.QuickAssistUserID = lines;
+            }
+        }
+        private void WriteQuickAutoLoginTextFile()
+        {
+            StreamWriter writer;
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            writer = File.CreateText(path + @"\QuickAutoLogin.txt");
+            writer.WriteLine(QuickAssistIDTextBox.Text);            
+            writer.Close();
+            QuickAssistProsess.QuickAssistUserID = QuickAssistIDTextBox.Text;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (timerCount >= 10)
+            {
+              
+                OtherPorcessCheck();
+                timerCount = 0;
+            }
+            else
+            {
+                timerCount = timerCount + 1;
+            }
+        }
+        private void OtherPorcessCheck()
+        {
+
+            Process[] processList = Process.GetProcessesByName("quickassist");
+            if (processList.Length < 1)
+            {
+                Clipboard.SetText(QuickAssistIDTextBox.Text);
+                Thread thread = new Thread(() => QuickAssistProsess.StartQuickAssist(QuickAssistIDTextBox.Text));
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();                
+               
+                timerCount = 0;
+            }
+        }
+
+        private void AutoQuickAssistButton_Click(object sender, EventArgs e)
+        {
+            WriteQuickAutoLoginTextFile();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(QuickAssistIDTextBox.Text);
+            Thread thread = new Thread(() => QuickAssistProsess.StartQuickAssist(QuickAssistIDTextBox.Text));
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+            timer1.Interval = 1000;
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);            
+        }
+       
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.Cancel != false) // 트레이 아이콘의 컨텍스트 메뉴를 통해 프로그램 종료가 선택된경우 true
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+                this.Visible = false; // 화면을 닫지 않고 단지 숨길 뿐이다. 
+            }
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem != null && e.ClickedItem.ToString().Equals("종료"))
+            {
+                this.Close();
+                this.Dispose();
+                Properties.Settings.Default.Save();
+                Application.Exit();
+            }
+            else if (e.ClickedItem != null && e.ClickedItem.ToString().Equals("활성화"))
+            {
+                this.Visible = true;
+            }
+        }
+
+    }
+}
---
+++ AutoQuickAssist/Mouse.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+
+namespace AutoQuickAssist
+{    public partial class Mouse
+
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        public int beforeMouseCursorX;
+        public int beforeMouseCursorY;
+
+
+        public static void Move(int xDelta, int yDelta)
+        {
+            mouse_event(MOUSEEVENTF_MOVE, xDelta, yDelta, 0, 0);
+        }
+        public static void MoveTo(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, x, y, 0, 0);
+        }
+        public void LeftClick()
+        {            
+            mouse_event(MOUSEEVENTF_LEFTDOWN, Control.MousePosition.X, Control.MousePosition.Y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, Control.MousePosition.X, Control.MousePosition.Y, 0, 0);
+            //mouse_event(MOUSEEVENTF_LEFTDOWN, clickPoint.X, clickPoint.Y, 0, 0);
+            //mouse_event(MOUSEEVENTF_LEFTUP, clickPoint.X, clickPoint.Y, 0, 0);
+        }
+
+        public  void LeftDown()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, Control.MousePosition.X, Control.MousePosition.Y, 0, 0);
+        }
+
+        public  void LeftUp()
+        {
+            mouse_event(MOUSEEVENTF_LEFTUP, Control.MousePosition.X, Control.MousePosition.Y, 0, 0);
+        }
+        public void SaveMouseCursor()
+        {
+             this.beforeMouseCursorX = Cursor.Position.X;
+             this.beforeMouseCursorY = Cursor.Position.Y;
+
+        }
+    }
+}
